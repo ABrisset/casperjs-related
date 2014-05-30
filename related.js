82f8e8e76c5583c5a84fdb90d721d07d101bb515
Antoine Brisset <ant.brisset@gmail.com>
2014-05-30 14:51:47 +0200

    Add README.rdoc and some useful comments to the code

--- related.js
+++ related.js
@@ -1,8 +1,13 @@
+// on instancie Casper
 var casper = require('casper').create({
     verbose: true,
 });
+
+// on récupère l'argument
 var query  = casper.cli.get(0);
 
+
+// on crée la fonction de scrape
 function getRelated() {
     var queries = document.querySelectorAll('.msrl a');
     return Array.prototype.map.call(queries, function(e) {
@@ -10,21 +15,24 @@ function getRelated() {
     });
 }
 
+// on quitte si aucun argument n'est fourni
 if (!query) {
     casper.echo('Tu as oublié le mot-clé !').exit(1);
 }
 
+// on recherche le mot-clé sur Google
 casper.start('http://www.google.fr/', function() {
     this.fillSelectors('form[action="/search"]', {
         'input[name="q"]': query,
     }, true);
 });
 
+// on attend une seconde puis on lance le scrape
 casper.wait(1000, function() {
     queries = this.evaluate(getRelated);
 });
 
-
+// on affiche les résultats en console
 casper.run(function() {
     this.echo(queries.length + ' recherches associées à ' + '"' + query + '"');
     if (queries.length > 1) {
